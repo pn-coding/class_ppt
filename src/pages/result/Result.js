fc9272c7014a48e3669fd95d1ef3659f0f37b3c8
pn-coding <pncoding@naver.com>
2024-12-20 14:36:00 +0900

    back btn

--- src/pages/result/Result.js
+++ src/pages/result/Result.js
@@ -1,37 +1,43 @@
 import { Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { routes } from "../../routes";
 
 export const Result = () => {
   let id = 0;
   return (
-    <Box
-      p="100px 150px 150px"
-      display={"grid"}
-      gridTemplateColumns={"repeat(3, 1fr)"}
-      columnGap={30}
-      rowGap={30}
-    >
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-      <Link to={`detail/${id}`}>
-        <Box h={400} bgColor={"lightgray"}></Box>
-      </Link>
-    </Box>
+    <>
+      <Box pos={"absolute"} top={5} left={50}>
+        <Link to={routes.home}>Home</Link>
+      </Box>
+      <Box
+        p="100px 150px 150px"
+        display={"grid"}
+        gridTemplateColumns={"repeat(3, 1fr)"}
+        columnGap={30}
+        rowGap={30}
+      >
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+        <Link to={`detail/${id}`}>
+          <Box h={400} bgColor={"lightgray"}></Box>
+        </Link>
+      </Box>
+    </>
   );
 };
