e0c80d60230e60234495cddc83c445176b825b9d
이정환 <sideway@ijeonghwan-ui-MacBookPro-2.local>
2024-12-23 12:05:38 +0900

    add img

--- src/pages/result/Result.js
+++ src/pages/result/Result.js
@@ -4,6 +4,7 @@ import { routes } from "../../routes";
 
 export const Result = () => {
   let id = 0;
+  const IMG_URL = process.env.PUBLIC_URL;
   return (
     <>
       <Box pos={"absolute"} top={5} left={50}>
@@ -13,29 +14,41 @@ export const Result = () => {
         p="100px 150px 150px"
         display={"grid"}
         gridTemplateColumns={"repeat(3, 1fr)"}
-        columnGap={30}
-        rowGap={30}
+        columnGap={50}
+        rowGap={100}
       >
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img src={`${IMG_URL}/img/seta_pc.jpg`} style={{ width: "100%" }} />
+          </Box>
         </Link>
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img
+              src={`${IMG_URL}/img/nonfiction_pc.jpg`}
+              style={{ width: "100%" }}
+            />
+          </Box>
         </Link>
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img src={`${IMG_URL}/img/LIG_pc.jpg`} style={{ width: "100%" }} />
+          </Box>
         </Link>
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img src={`${IMG_URL}/img/cup_pc.jpg`} style={{ width: "100%" }} />
+          </Box>
         </Link>
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img src={`${IMG_URL}/img/st.jpg`} style={{ width: "100%" }} />
+          </Box>
         </Link>
         <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
-        </Link>
-        <Link to={`detail/${id}`}>
-          <Box h={400} bgColor={"lightgray"}></Box>
+          <Box bgColor={"lightgray"} boxShadow="0 0 10px rgba(0,0,0,0.2)">
+            <img src={`${IMG_URL}/img/apt.jpg`} style={{ width: "100%" }} />
+          </Box>
         </Link>
       </Box>
     </>
